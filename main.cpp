d9ab30765d3fef26cf8e43c8e5c068df66ff9c3e
galyaminavas <galyaminavas@gmail.com>
2020-11-30 11:38:58 +0300

    Added Google_tests and made refactoring

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <time.h>
 #include <algorithm>
 #include <vector>
-#include "sortsPointers.h"
-#include "sortsPointers.cpp"
+#include "Sorts/sortsPointers.h"
 
 using namespace std;
 
@@ -43,31 +42,63 @@ void calcTime() {
 }
 
 int main() {
-//    const int arraySize = 7;
-//    int unsortedArray[arraySize] = {4, 3, 12, 7, 8, 1, 0};
-//    sort(&unsortedArray[0], &unsortedArray[arraySize - 1], compare<int>);
-
-//    const int arraySize = 8;
-//    int unsortedArray[arraySize] = {2, 1, 8, 6, 4, 3, 9, 7};
-//    sort(&unsortedArray[0], &unsortedArray[arraySize - 1], compare<int>);
-
-//    const int arraySize = 8;
-//    float unsortedArray[arraySize] = {2.0, 2.0, 8.0, 6.5, 4.0, 3.0, 9.2, 7.1};
-//    sort(&unsortedArray[0], &unsortedArray[arraySize - 1], [](float a, float b) { return a <= b; });
-////    sort(&unsortedArray[0], &unsortedArray[arraySize - 1], compare<float>);
-
-    const int arraySize = 17;
-    char unsortedArray[arraySize] = "defnoijklmcpabgh";
-
-    for (int i = 0; i < arraySize; i++) {
-        cout << unsortedArray[i] << " ";
+    const int arraySize1 = 7;
+    int unsortedArray1[arraySize1] = {4, 3, 12, 7, 8, 1, 0};
+    for (int i = 0; i < arraySize1; i++) {
+        cout << unsortedArray1[i] << " ";
     }
     cout << endl;
+    sort(&unsortedArray1[0], &unsortedArray1[arraySize1 - 1], compare<int>);
+    for (int i = 0; i < arraySize1; i++) {
+        cout << unsortedArray1[i] << " ";
+    }
+    cout << endl << endl;
 
-    sort(&unsortedArray[0], &unsortedArray[arraySize - 2], compare<char>);
+    const int arraySize2 = 8;
+    int unsortedArray2[arraySize2] = {2, 1, 8, 6, 4, 3, 9, 7};
+    for (int i = 0; i < arraySize2; i++) {
+        cout << unsortedArray2[i] << " ";
+    }
+    cout << endl;
+    sort(&unsortedArray2[0], &unsortedArray2[arraySize2 - 1], compare<int>);
+    for (int i = 0; i < arraySize2; i++) {
+        cout << unsortedArray2[i] << " ";
+    }
+    cout << endl << endl;
 
-    for (int i = 0; i < arraySize; i++) {
-        cout << unsortedArray[i] << " ";
+    const int arraySize3 = 8;
+    float unsortedArray3[arraySize3] = {2.0, 2.0, 8.0, 6.5, 4.0, 3.0, 9.2, 7.1};
+    for (int i = 0; i < arraySize3; i++) {
+        cout << unsortedArray3[i] << " ";
+    }
+    cout << endl;
+    sort(&unsortedArray3[0], &unsortedArray3[arraySize3 - 1], [](float a, float b) { return a <= b; });
+    for (int i = 0; i < arraySize3; i++) {
+        cout << unsortedArray3[i] << " ";
+    }
+    cout << endl << endl;
+
+    const int arraySize4 = 8;
+    float unsortedArray4[arraySize4] = {2.0, 2.0, 8.0, 6.5, 4.0, 3.0, -9.2, 0.0};
+    for (int i = 0; i < arraySize4; i++) {
+        cout << unsortedArray4[i] << " ";
+    }
+    cout << endl;
+    sort(&unsortedArray4[0], &unsortedArray4[arraySize4 - 1], compare<float>);
+    for (int i = 0; i < arraySize4; i++) {
+        cout << unsortedArray4[i] << " ";
+    }
+    cout << endl << endl;
+
+    const int arraySize5 = 17;
+    char unsortedArray5[arraySize5] = "defnoijklmcpabgh";
+    for (int i = 0; i < arraySize5; i++) {
+        cout << unsortedArray5[i] << " ";
+    }
+    cout << endl;
+    sort(&unsortedArray5[0], &unsortedArray5[arraySize5 - 2], compare<char>);
+    for (int i = 0; i < arraySize5; i++) {
+        cout << unsortedArray5[i] << " ";
     }
 
     return 0;
